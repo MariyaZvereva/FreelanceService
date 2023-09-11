d73882119fe1010dccc6b42f904052395d4c5fa2
Marya Zvereva <mashechka28@mail.ru>
2023-09-11 21:37:02 +0300

    first commit

---
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FreelanceServiceTest {
+    @Test
+    public void testCalculate1() {
+        FreelanceService service = new FreelanceService();
+
+        int actual = service.calculate(20_000, 3_000, 25_000);
+        int expected = 10;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculate2() {
+        FreelanceService service = new FreelanceService();
+
+        int actual = service.calculate(200_000, 60_000, 100_000);
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
